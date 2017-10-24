c66ba00baa0d8451fdb802eab660a9ad350169b1
thelark <mperial@foxmail.com>
2017-10-24 16:34:36 +0800

    New
    + cgi.go
    + server.go

---
+++ cgi.go
@@ -0,0 +1,45 @@
+package cgi
+
+import (
+	"net/http/cgi"
+	"os"
+)
+
+var (
+	GOROOT = os.Getenv("GOROOT")
+	GOPATH = os.Getenv("GOPATH")
+	GOEXE  = GOROOT + "/bin/go"
+)
+
+type CGI struct {
+	runPath string // eg."D:/workspace/go/src/thelark.cn/cgi" 运行路径
+	goExe   string // eg."E:/Go/bin/go" 脚本路径
+	goRoot  string
+	goPath  string
+	*cgi.Handler
+}
+
+func New(runPath string) *CGI {
+	return &CGI{runPath: runPath, goExe: GOEXE, goRoot: GOROOT, goPath: GOPATH, Handler: new(cgi.Handler)}
+}
+func (c *CGI) SetGoRoot(goRoot string) {
+	c.goRoot = goRoot
+	c.goExe = goRoot + "/bin/go"
+}
+func (c *CGI) GetGoRoot() string {
+	return c.goRoot
+}
+func (c *CGI) SetGoPath(goPath string) {
+	c.goPath = goPath
+}
+func (c *CGI) GetGoPath() string {
+	return c.goPath
+}
+func (c *CGI) SetGoExe(goExe string) {
+	c.goExe = goExe
+}
+func (c *CGI) GetGoExe() string {
+	return c.goExe
+}
+
+
